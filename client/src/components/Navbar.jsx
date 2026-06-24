d1b825613ad88587708392781ef77f948be7d272
Pawan Bhatt <2712chandra@gmail.com>
2026-06-24 20:39:16 +0530

    Navbar updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { motion } from "motion/react";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 import Logo from "../assets/logo.png";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
-    const {userData} = useSelector((state)=> state.user)
-    const credits = userData.user.credits
-    console.log(credits)
+  const { userData } = useSelector((state) => state.user);
+  const credits = userData.user.credits;
+  // console.log(credits);
+  const [showCredits, setShowCredits] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
 
   return (
     <motion.div
@@ -22,7 +24,85 @@ const Navbar = () => {
         </span>
       </div>
       <div className="flex items-center gap-6 relative">
-        <div className="relative"></div>
+        {/* show-credits-button-start */}
+        <div className="relative">
+          <motion.div
+            onClick={() => {
+              setShowCredits(!showCredits);
+              setShowProfile(false);
+            }}
+            whileHover={{ scale: 1.07 }}
+            whileTap={{ scale: 0.97 }}
+            className="flex items-center gap-1 px-2 py-2 rounded-full bg-white/10 border border-white/20 text-white text-sm shadow-md cursor-pointer"
+          >
+            <span className="text-xl">💠</span>
+            <span>{credits}</span>
+            <motion.span
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.97 }}
+              className="ml-2 h-5 w-5 flex items-center justify-center rounded-full bg-white text-xs font-bold"
+            >
+              ➕
+            </motion.span>
+          </motion.div>
+          {/* show-add-more-credits-dialog-box-start */}
+          <AnimatePresence>
+            {showCredits && (
+              <motion.div
+                initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 1, scale: 1 }}
+                exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                transition={{ duration: 0.4 }}
+                className="absolute right-[-50px] mt-4 w-64 rounded-2xl bg-black/90 backdrop-blur-xl border border-white/10 shadow-[0_25px_60px_rgba(0,0,0,0.7)] p-4 text-white"
+              >
+                <h4 className="font-semibold mb-2">Buy Credits</h4>
+                <p className="text-sm text-gray-300 mb-4">
+                  Use credits to generate AI notes, diagrams & PDFs.
+                </p>
+                <button
+                  onClick={() => setShowCredits(!showCredits)}
+                  className="w-full py-2 rounded-lg bg-gradient-to-br from-white to-gray-200 text-black font-semibold hover:opacity-90"
+                >
+                  Buy More Credits
+                </button>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          {/* show-add-more-credits-dialog-box-end */}
+        </div>
+        {/* show-credits-button-end */}
+
+        {/* profile-icon-start */}
+        <div className="relative">
+          <motion.div
+            onClick={() => {
+              setShowProfile(!showProfile);
+              setShowCredits(false);
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.97 }}
+            className="flex items-center gap-1 px-2 py-2 rounded-full bg-white/10 border border-white/20 text-white text-sm shadow-md cursor-pointer"
+          >
+            <span className="text-md">
+              {userData?.user?.name.slice(0, 1).toUpperCase()}
+            </span>
+          </motion.div>
+
+          {/* show-profile-dropdown-start */}
+          <AnimatePresence>
+            {showProfile && (
+              <motion.div
+                initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 1, scale: 1 }}
+                exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                transition={{ duration: 0.4 }}
+                className="absolute right-0 mt-4 w-52 rounded-2xl bg-black/90 backdrop-blur-xl border border-white/10 shadow-[0_25px_60px_rgba(0,0,0,0.7)] p-4 text-white"
+              ></motion.div>
+            )}
+          </AnimatePresence>
+          {/* show-profile-dropdown-end */}
+        </div>
+        {/* profile-icon-end */}
       </div>
     </motion.div>
   );
